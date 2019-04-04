41e9a90e52ac0e7dd36f4e9363d96d2fa94349ff
hao yuan <913942870@qq.com>
2019-04-05 00:24:30 +1100

    h

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import {TestComponent} from './test/test.component';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: TestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
