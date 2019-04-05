877ae74d464569fc26814e1cccc675209fe51416
Hongning Shangguan <hongningsg@yahoo.com>
2019-04-05 22:55:23 +1100

    continous analysis done, score can matching with discrete

--- service/src/utils/__init__.py
+++ service/src/utils/__init__.py
@@ -1,5 +1,6 @@
-from methods import entropy
+from methods import Logistic, DecisionTree
 import numpy as np
+import pandas as pd
 
 
 discrete_data = [
@@ -11,22 +12,47 @@ continuous_data = [
 ]
 
 
-def discrete_analysis(data, target, data_type):
+# def discrete_analysis(data, target, data_type):
+#     is_dicrete = data_type in discrete_data
+#     if is_dicrete:
+#         positive_data = {}
+#         for i in range(len(data)):
+#             if target[i] != 0:
+#                 try:
+#                     positive_data[data[i]] += 1
+#                 except:
+#                     positive_data[data[i]] = 1
+#         data_distribution = []
+#         for key in positive_data:
+#             data_distribution.append(positive_data[key])
+#         np_data = np.array(data_distribution)
+#         score =  entropy(np_data)
+#         return score
+#     else:
+#         return -1
+
+def discrete_analysis(data, target, data_type, interpreter = {}):
     is_dicrete = data_type in discrete_data
     if is_dicrete:
-        positive_data = {}
-        for i in range(len(data)):
-            if target[i] != 0:
-                try:
-                    positive_data[data[i]] += 1
-                except:
-                    positive_data[data[i]] = 1
-        data_distribution = []
-        for key in positive_data:
-            data_distribution.append(positive_data[key])
-        np_data = np.array(data_distribution)
-        score =  entropy(np_data)
-        return score
+        one_hot_raw ={}
+        for key in interpreter:
+            one_hot_raw[key] = []
+        for x in data:
+            for key in one_hot_raw:
+                if key == x:
+                    one_hot_raw[key].append(1)
+                else:
+                    one_hot_raw[key].append(0)
+        y = np.array(target)
+        X = pd.DataFrame(map)
+        return DecisionTree(X, y)
     else:
         return -1
 
+def continous_analysis(data, target, data_type):
+    is_continous = data_type in continuous_data
+    if not is_continous:
+        return -1
+    X = np.array(data)
+    y = np.array(target)
+    return Logistic(X, y)
--- service/src/utils/methods.py
+++ service/src/utils/methods.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
 
 def entropy(data):
     base = np.log(data.shape[0])
@@ -16,23 +17,14 @@ def entropy(data):
     H_x = prob * (log_prob/base)
     return -np.sum(H_x)
 
+def Logistic(data, target):
+    n, m = data.shape
+    data = target.reshape(m, n)
+    clf = LogisticRegression(solver='liblinear')
+    scores = cross_val_score(clf, data, target, cv=10)
+    return np.mean(scores)
 
-def logistic_train(data, target):
-    clf = LogisticRegression(random_state=0, solver='lbfgs',
-                             multi_class='multinomial').fit(data, target)
-    return clf
-
-def pred(clf, data):
-    return clf.predict(data)
-
-X = [1, 1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1, 1, 1, 1, 1, 1, 1]
-y = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-X = np.array(X).reshape(26, 1)
-y = np.array(y)
-
-clf = LogisticRegression(random_state=0, solver='lbfgs',
-                             multi_class='multinomial')
-scores = cross_val_score(clf, X, y, cv=6)
-print(scores)
-# test = np.array([3, 4, 5, 12, 23, 111]).reshape(6, 1)
-# print(pred(clf, test))
+def DecisionTree(data, target):
+    clf = DecisionTreeClassifier(criterion="entropy")
+    scores = cross_val_score(clf, data, target, cv=10)
+    return np.mean(scores)
